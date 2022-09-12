6f57eeabbf925425e5fe60ad4fa950ecc1ccc433
Abdusattorova <113165267+Abdusattorova@users.noreply.github.com>
2022-09-12 10:21:12 +0500

    Gradle ga  build Main A class

--- app/src/main/java/com/example/lesson_9/MainActivity.kt
+++ app/src/main/java/com/example/lesson_9/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.lesson_9
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lesson_9.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+}
+
+class A {
+
 }
